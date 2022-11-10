68723a507ba930f5851198584f4bdb393324d58f
DESKTOP-SPTMR3U\魚鬆 <tknifegame@gmail.com>
2022-11-10 22:45:53 +0800

    SlidingPuzzle 拼圖解完功能判定

--- Assets/Scripts/SlidingPuzzle.cs
+++ Assets/Scripts/SlidingPuzzle.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SlidingPuzzle : MonoBehaviour
 {
-
     public Texture2D puzzleImage;       // 謎題貼圖
 	public int puzzleGridX = 3;         // 格子X軸數量
 	public int puzzleGridY = 3;         // 格子Y軸數量
@@ -14,28 +13,78 @@ public class SlidingPuzzle : MonoBehaviour
     private GameObject[,] tileObjectArray;      // 方塊物件清單
 	private Vector3[,] tilePosArray;            // 方塊座標陣列
 	private SlidingPuzzleTile emptyTile;        // 空方塊
+    private bool isPuzzleActive = false;        // 謎題是否開始
 
     // 生命週期 --------------------------------------------------------------------------------------------------------------
     // Start is called before the first frame update
     void Start()
     {
+        isPuzzleActive = false;
         if (puzzleImage) {
-            CreatePuzzleTiles();
-            jugglePuzzle();
+            startPuzzle();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isPuzzleActive) {
+            return;
+        }
+        onMouseDown();
+        onTouchDown();
+    }
+
+    // 初始化
+    public void init(Texture2D image, int gridX, int gridY) {
+        puzzleImage = image;
+        puzzleGridX = gridX;
+        puzzleGridY = gridY;
+        startPuzzle();
+    }
+
+    // 開始遊戲
+    public void startPuzzle() {
+        createPuzzleTiles();
+        jugglePuzzle();
+        isPuzzleActive = true;
+    }
+
+    // 結束遊戲
+    public void finishPuzzle() {
+        isPuzzleActive = false;
+        emptyTile.gameObject.SetActive(true);
+        Debug.Log("Puzzle complete!");
+    }
+
+    // 觸碰處理 --------------------------------------------------------------------------------------------------------------
+
+    /** 滑鼠觸碰 */
+    void onMouseDown() {
         if (Input.GetMouseButtonDown(0)) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
             if (hit) {
-                SlidingPuzzleTile tmepTile = hit.transform.gameObject.GetComponent<SlidingPuzzleTile>();
-                if (tmepTile) {
-                    moveTileToEmptyPos(tmepTile);
-                }
+                handleTileHit(hit);
+            }
+        }
+    }
+
+    /** 手機觸碰 */
+    void onTouchDown() {
+        // TODO: 手機觸碰
+    }
+
+    /** 處理方塊碰撞 */
+    private void handleTileHit(RaycastHit2D hit) {
+        if (!hit) {
+            return;
+        }
+        SlidingPuzzleTile tmepTile = hit.transform.gameObject.GetComponent<SlidingPuzzleTile>();
+        if (tmepTile) {
+            moveTileToEmptyPos(tmepTile);
+            if (checkPuzzleComplete()) {
+                finishPuzzle();
             }
         }
     }
@@ -43,7 +92,7 @@ public class SlidingPuzzle : MonoBehaviour
     // 內部呼叫 --------------------------------------------------------------------------------------------------------------
 
     /** 創造謎題方塊 */
-    private void CreatePuzzleTiles()
+    private void createPuzzleTiles()
     {
 	    Vector3 position;
         SpriteRenderer tileSpriteRenderer = tile.GetComponent<SpriteRenderer>();
@@ -90,7 +139,6 @@ public class SlidingPuzzle : MonoBehaviour
         SlidingPuzzleTile tmepTile;
 
         emptyTile = tmepObject.GetComponent<SlidingPuzzleTile>();
-        emptyTile.setGameActive(false);
         emptyTile.gameObject.SetActive(false);
         //Debug.Log(emptyTile.getNowGridPos());
 
@@ -104,16 +152,6 @@ public class SlidingPuzzle : MonoBehaviour
             }
 		}
     }
-
-    /** 判斷方塊是否可移動 */
-    private bool checkTileCanMove(SlidingPuzzleTile thisTile)
-	{
-        if (thisTile != emptyTile
-        && Vector2.Distance(thisTile.getNowGridPos(), emptyTile.getNowGridPos()) == 1) {
-            return true;
-        }
-		return false;
-	}
 
     /** 移動方塊到空位置 */
     private bool moveTileToEmptyPos(SlidingPuzzleTile thisTile) {
@@ -131,6 +169,35 @@ public class SlidingPuzzle : MonoBehaviour
             thisTile.setNowGridPos(tempPos);
             emptyTile.setNowGridPos(targetPos);
             //Debug.Log(emptyTile.getNowGridPos());
+            return true;
+        }
+        return false;
+    }
+
+    /** 檢查方塊是否可移動 */
+    private bool checkTileCanMove(SlidingPuzzleTile thisTile)
+	{
+        if (thisTile != emptyTile
+        && Vector2.Distance(thisTile.getNowGridPos(), emptyTile.getNowGridPos()) == 1) {
+            return true;
+        }
+		return false;
+	}
+
+    /** 檢查是否獲勝 */
+    private bool checkPuzzleComplete() {
+        int completeCount = puzzleGridX * puzzleGridY;
+        SlidingPuzzleTile tmepTile;
+
+        for(int j = 0; j < puzzleGridX; j++){
+			for(int i = 0; i < puzzleGridY; i++) {
+                tmepTile = tileObjectArray[i, j].GetComponent<SlidingPuzzleTile>();
+                if (tmepTile.checkGridCorrect()) {
+                    completeCount--;
+                }
+            }
+        }
+        if (completeCount <= 0) {
             return true;
         }
         return false;
--- Assets/Scripts/SlidingPuzzleTile.cs
+++ Assets/Scripts/SlidingPuzzleTile.cs
@@ -6,24 +6,18 @@ public class SlidingPuzzleTile : MonoBehaviour
 {
     private Vector2Int goalGridPos = new Vector2Int();    // 目標格子位置
 	private Vector2Int nowGridPos = new Vector2Int();     // 目前格子位置
-    private bool isCorrectGrid = false;             // 是否在正確的位置
-    private bool isGameActive = false;              // 是否正在遊戲中
     
     // 生命週期 --------------------------------------------------------------------------------------------------------------
     // Start is called before the first frame update
     void Start()
     {
-        isCorrectGrid = false;
-        isGameActive = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isGameActive) {
-            return;
-        }
-        isCorrectGrid = checkGridCorrect();
+
     }
 
     // 初始化
@@ -49,14 +43,9 @@ public class SlidingPuzzleTile : MonoBehaviour
         return nowGridPos;
     }
 
-    /** 設定正在遊戲中 */
-    public void setGameActive(bool isActive) {
-        isGameActive = isActive;
-    }
-
-    /** 取得正在遊戲中 */
-    public bool getGameActive() {
-        return isGameActive;
+    /** 檢查格子是否在正確的位置 */
+    public bool checkGridCorrect() {
+        return (nowGridPos == goalGridPos);
     }
 
     // 內部呼叫 --------------------------------------------------------------------------------------------------------------
@@ -64,10 +53,5 @@ public class SlidingPuzzleTile : MonoBehaviour
     /** 設定目標格子位置 */
     private void setGoalGridPos(Vector2Int pos) {
         goalGridPos = pos;
-    }
-
-    /** 檢查格子是否在正確的位置 */
-    private bool checkGridCorrect() {
-        return (nowGridPos == goalGridPos);
     }
 }
