344f259be51c2eb8e3f171f82acf4b8a378aab28
DESKTOP-SPTMR3U\魚鬆 <tknifegame@gmail.com>
2022-12-01 18:04:11 +0800

    Typewriter 解析並分段句子

--- Assets/Scripts/DialogBox.cs
+++ Assets/Scripts/DialogBox.cs
@@ -35,14 +35,14 @@ public class DialogBox : MonoBehaviour
         btnNext.onClick.AddListener(onClickDialogBox);
         // ---------------------------------------
         // TODO: 測試用
-        messageList = new List<string>();
-        addMessageToList("聖誕前夜，到處洋溢著歡樂的氣氛，但對TAKAMORI來説卻是例外");
-        addMessageToList("Calli和Kiara之間充滿古怪與尷尬的氣氛，但她們之前並沒有吵過架");
-        addMessageToList("Kiara最近總是很晚才回家，而且常常嘆氣，但當Calli試著跟她說話時\n她的反應卻總是一切都好");
-        addMessageToList("Calli告訴她自己他們之間不能再這樣了，");
-        addMessageToList("她擔心是因為自己太傲嬌讓Kiara忍受不了，想要離開她");
-        addMessageToList("Calli: 我想讓她開心起來，但是我該怎麼做？");
-        isReadyPlay = true;
+        string testText = "";
+        testText = testText + "聖誕前夜，到處洋溢著歡樂的氣氛，但對TAKAMORI來説卻是例外<block>";
+        testText = testText + "Calli和Kiara之間充滿古怪與尷尬的氣氛，但她們之前並沒有吵過架<block>";
+        testText = testText + "Kiara最近總是很晚才回家，而且常常嘆氣，但當Calli試著跟她說話時\n她的反應卻總是一切都好<block>";
+        testText = testText + "Calli告訴她自己他們之間不能再這樣了，<block>";
+        testText = testText + "她擔心是因為自己太傲嬌讓Kiara忍受不了，想要離開她<block>";
+        testText = testText + "Calli: 我想讓她開心起來，但是我該怎麼做？";
+        setMessageData(testText);
         // var func = StartCoroutine(test());
         // ---------------------------------------
     }
@@ -96,7 +96,9 @@ public class DialogBox : MonoBehaviour
     /** 設定全部訊息 */
     public void setMessageData(string text) {
         messageList = new List<string>();
-        parseMessage(text);
+        if (parseMessage(text)) {
+            isReadyPlay = true;
+        }
     }
 
     /** 增加選項 */
@@ -140,10 +142,23 @@ public class DialogBox : MonoBehaviour
     }
 
     /** 解析訊息成陣列 */
-    private void parseMessage(string text) {
-        // TODO: 解析句子檢查有無分段標記
-        addMessageToList(text);
-        isReadyPlay = true;
+    private bool parseMessage(string text) {
+        string tempStr = text;
+        int index = 0;
+        while(tempStr.Length > 0) {
+            index = tempStr.IndexOf(newlineChar);
+            if (index > 0) {
+                string result = tempStr.Substring(0, index);
+                addMessageToList(result);
+                tempStr = tempStr.Remove(0, index + newlineChar.Length);
+            }
+            else {
+                addMessageToList(tempStr);
+                tempStr = tempStr.Remove(0, tempStr.Length - 1);
+                return true;
+            }
+        }
+        return false;
     }
 
     /** 增加對話到陣列 */
