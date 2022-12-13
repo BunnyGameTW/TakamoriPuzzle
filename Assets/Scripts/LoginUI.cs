e085455493a2563b58624e1882e2d96d82c9b48b
Angel Cheng <angelcheng0916@gmail.com>
2022-12-14 02:42:30 +0800

    新增顯示進度、調整共用按鈕位置、修改設定頁面排版跳過遊玩、自動對話

---
+++ Assets/Scripts/LoginUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class LoginUI : MonoBehaviour
+{
+    public GameObject gameObjectProgress;
+    public TextMeshProUGUI textProgress;
+    const string PROGRESS_STRING_FORMAT = "Ep.{0}-{1}";
+    // Start is called before the first frame update
+    void Start()
+    {
+        int episodeId = DataManager.instance.getEpisodeId();
+        int levelId = DataManager.instance.getLevelId();
+        bool needShowProgress = episodeId != 0 && levelId != 0;
+        if (gameObjectProgress.activeSelf != needShowProgress)
+            gameObjectProgress.SetActive(needShowProgress);
+        if (needShowProgress)
+            textProgress.text = string.Format(PROGRESS_STRING_FORMAT, episodeId, levelId);        
+    }
+}
