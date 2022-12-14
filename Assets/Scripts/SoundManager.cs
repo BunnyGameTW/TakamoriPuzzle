bb5fe35c3b0266ea703e2937f2e91c6c41996614
DESKTOP-SPTMR3U\魚鬆 <tknifegame@gmail.com>
2022-12-14 22:52:17 +0800

    SoundManager 音樂淡入淡出

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -21,6 +21,10 @@ public class SoundManager: SingletonMono<SoundManager>
     public AudioClip SE_transitionIn;
     public AudioClip SE_transitionOut;
 
+    private float fadeInDuration = 0.75f;
+    private float fadeOutDuration = 0.75f;
+    private Coroutine BMGEvent = null;
+
     // 生命週期 --------------------------------------------------------------------------------------------------------------
 
     // Start is called before the first frame update
@@ -38,13 +42,24 @@ public class SoundManager: SingletonMono<SoundManager>
     
     /** 播放音樂 */
     public void playBGM(AudioClip music) {
-        if (musicSource.isPlaying == true) {
-            musicSource.Stop();
+        if (music == musicSource.clip) {
+            return;
         }
-        musicSource.clip = music;
-        musicSource.loop = true;
-        musicSource.volume = DataManager.instance.BGMVolime;
-        musicSource.Play();
+        if (BMGEvent != null) {
+            StopCoroutine(BMGEvent);
+        }
+        BMGEvent = StartCoroutine(changeBGM(music));
+    }
+
+    /** 停止音樂 */
+    public void stopBGM(AudioClip music) {
+        if (musicSource.isPlaying == false) {
+            return;
+        }
+        if (BMGEvent != null) {
+            StopCoroutine(BMGEvent);
+        }
+        BMGEvent = StartCoroutine(fadeOutStopBGM());
     }
 
     /** 播放音效 */
@@ -61,4 +76,40 @@ public class SoundManager: SingletonMono<SoundManager>
     public void setSEVolime(float value) {
         effectSource.volume = value;
     }
+
+    // 內部呼叫 --------------------------------------------------------------------------------------------------------------
+
+    /** 切換音樂 */
+    private IEnumerator changeBGM(AudioClip music) {
+        if (musicSource.isPlaying == true) {
+            yield return fadeBGM(musicSource.volume, 0, fadeOutDuration);
+        }
+        musicSource.clip = music;
+        musicSource.loop = true;
+        musicSource.Play();
+        yield return fadeBGM(0, DataManager.instance.BGMVolime, fadeInDuration);
+        BMGEvent = null;
+        yield break;
+    }
+
+    /** 淡出停止音樂 */
+    private IEnumerator fadeOutStopBGM() {
+        if (musicSource.isPlaying == true) {
+            yield return fadeBGM(musicSource.volume, 0, fadeOutDuration);
+        }
+        musicSource.Stop();
+        yield break;
+    }
+
+    /** 淡入淡出音樂 */
+    private IEnumerator fadeBGM(float beginVolume, float finalVolume, float duration) {
+        float currentTime = 0;
+        while (currentTime < duration) {
+            currentTime += Time.deltaTime;
+            musicSource.volume = Mathf.Lerp(beginVolume, finalVolume, Easing.Tween(currentTime/duration, EASE_TYPE.Linear));
+            yield return null;
+        }
+        yield break;
+    }
+
 }
